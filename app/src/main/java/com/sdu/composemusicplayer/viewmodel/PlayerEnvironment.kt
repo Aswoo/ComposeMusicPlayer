a82d2708fb67597a6f5c9e8873b2090c1328dbf8
Aswoo <yhnu191919@gmail.com>
2024-12-23 11:57:31 +0900

    editconfig test

--- app/src/main/java/com/sdu/composemusicplayer/viewmodel/PlayerEnvironment.kt
+++ app/src/main/java/com/sdu/composemusicplayer/viewmodel/PlayerEnvironment.kt
@@ -29,7 +29,7 @@ class PlayerEnvironment
     constructor(
         @ApplicationContext private val context: Context,
         private val musicRepository: MusicRepository,
-        private val exoPlayer: ExoPlayer,
+        private val exoPlayer: ExoPlayer, //klint test
     ) : IPlayerEnvironment {
         val dispatcher: CoroutineDispatcher = Dispatchers.IO
 
